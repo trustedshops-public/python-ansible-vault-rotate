fe0e66f00271dfef8f362fec0b9612b61bbfbc8f
Timo Reymann <Timo.Reymann@trustedshops.de>
2023-01-09 19:13:07 +0100

    chore: Add test for vaulted file

--- ansible_vault_rotate/vault/file_test.py
+++ ansible_vault_rotate/vault/file_test.py
@@ -1,16 +1,28 @@
 import os
 import unittest
 from tempfile import NamedTemporaryFile
+
+from ansible.parsing.vault import VaultEditor
+
 from ansible_vault_rotate.vault import load_with_vault
 from .file import rekey_file
+from .util import create_vault_lib
 
 
 class VaultFileTest(unittest.TestCase):
+    def fixture_name(self, file_name: str) -> str:
+        return os.path.dirname(os.path.abspath(__file__)) + f"/__testdata__/{file_name}"
+
+    def assertLineCount(self, f: NamedTemporaryFile, count: int):
+        lines = f.readlines()
+        self.assertEqual(len(lines), count)
+        f.seek(0)
+
     def test_single_secret(self):
         with NamedTemporaryFile("r", delete=True) as f:
-            rekey_file(os.path.dirname(os.path.abspath(__file__)) + "/__testdata__/single-secret.yml", "test", "test123", f.name)
-            lines = f.readlines()
-            self.assertEqual(len(lines), 8)
+            rekey_file(self.fixture_name("single-secret.yml"), "test", "test123", f.name)
+
+            self.assertLineCount(f, 8)
 
             doc = load_with_vault(f.name, "default", "test123")
             self.assertEqual(doc['regular_key'], "goes here")
@@ -18,11 +30,21 @@ class VaultFileTest(unittest.TestCase):
 
     def test_multiple_secret(self):
         with NamedTemporaryFile("r", delete=True) as f:
-            rekey_file(os.path.dirname(os.path.abspath(__file__)) + "/__testdata__/multiple-secret.yml", "test", "test123", f.name)
-            lines = f.readlines()
-            self.assertEqual(len(lines), 17)
+            rekey_file(self.fixture_name("multiple-secret.yml"), "test", "test123", f.name)
+
+            self.assertLineCount(f, 17)
 
             doc = load_with_vault(f.name, "default", "test123")
             self.assertEqual(doc['regular_key'], "goes here")
             self.assertEqual(doc['my_key']['a'], 'test')
             self.assertEqual(doc['my_key']['b'], 'abc')
+
+    def test_vault_file(self):
+        with NamedTemporaryFile("r", delete=True) as f:
+            rekey_file(self.fixture_name("vaulted-file.txt"), "test", "test123", f.name)
+
+            self.assertLineCount(f, 6)
+
+            editor = VaultEditor(create_vault_lib("default", "test123"))
+            content = editor.plaintext(f.name).decode("utf8")
+            self.assertEqual(content, "i am\na multiline\nstring\n")
